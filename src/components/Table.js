14256caa392a422841f0aaad00f0db1d6320ae3e
cliffmandagi <55742770+cliffmandagi@users.noreply.github.com>
2020-12-07 23:35:41 +0800

    Add Table Components
    12/7/2020

---
+++ src/components/Table.js
@@ -0,0 +1,19 @@
+import react from "react";
+import { Table } from "react-bootstrap";
+
+const Table = () => {
+  return (
+    <div>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+    </div>
+  );
+};
+
+export default Table;
